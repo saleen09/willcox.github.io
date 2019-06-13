f38b3a14e56bfdf9372e9cbd804f4b186c170cef
saleen09 <saleen09@hotmail.com>
2019-06-12 21:48:28 -0700

    1

---
+++ assignments/lesson-8/js/windchill.js
@@ -0,0 +1,15 @@
+
+function wchillnow() {
+var wchill=35.74 + 0.6215*otemp +(0.4275*otemp - 35.75)*Math.pow(wspeed,0.16);
+}
+
+
+
+
+/*<!-- 35.74 + 0.6215T - 35.75V (**0.16) + 0.4275TV(**0.16)
+
+f = 35.75 + 0.6215t - 35.75s^0.16 + 0.4275 t s^0.16
+
+where f is the wind chill factor in Fahrenheit
+t is the air average temperature in Fahrenheit
+and s is the wind speed in miles per hour. -->  */
