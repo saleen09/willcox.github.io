ac9388572647681d416749313916eedc95c94ad2
saleen09 <saleen09@hotmail.com>
2019-06-22 21:35:57 -0700

    1

--- assignments/lesson-9/js/towndata.mod.js
+++ assignments/lesson-9/js/towndata.mod.js
@@ -10,11 +10,11 @@ request.send();
 
 request.onload = function () {
     var tdata = request.response;
-/*   populateHeader(tdata);
+    populateHeader(tdata);
     showTowns(tdata);
 }
 
-function populateHeader(jsonObj) {
+/*function populateHeader(jsonObj) {
     var myH1 = document.createElement('h1');
     myH1.textContent = jsonObj['squadName'];
     header.appendChild(myH1); -->
@@ -24,19 +24,12 @@ function populateHeader(jsonObj) {
     header.appendChild(myPara);
 } */
 
-}
-
 function showTowns(jsonObj) {
     var towns = jsonObj['towns'];
-    var townNames = ["Fish Haven", "Soda Springs", "Preston"]
 
-    for (var i = 0; i < towns.length; i++)
-    for (var x = 0; x < townnames.length; x++){
-        if (towns[i].name == townNames[x])
-
-     {
-        /* var myArticle = document.createElement('article');*/
-        var myH2 = document.createElement('h2');
+    for (var i = 0; i < towns.length; i++) {
+        /* var myArticle = document.createElement('article');
+        < !––var myH2 = document.createElement('h2');*/
         var myTown1 = document.createElement('p');
         var myTown2 = document.createElement('p');
         var myTown3 = document.createElement('p');
@@ -53,17 +46,17 @@ function showTowns(jsonObj) {
         for (var j = 0; j < superPowers.length; j++) {
             var listItem = document.createElement('li');
             listItem.textContent = superPowers[j];
-            myList.appendChild(listItem);*/
+            myList.appendChild(listItem);
         }
 
         myArticle.appendChild(myH2);
-        myArticle.appendChild(myTown1);
-        myArticle.appendChild(myTown2);
-        myArticle.appendChild(myTown3);
-        myArticle.appendChild(myTown4);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
 
         section.appendChild(myArticle);
-    }
+    }*/
     }
 
-
+}
