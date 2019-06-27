cfb1d158a810dd6459a9627ec6a423908a949757
saleen09 <saleen09@hotmail.com>
2019-06-26 21:50:10 -0700

    1

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -1,8 +1,7 @@
 
 let weatherRequest = new XMLHttpRequest();
-weatherRequest.open("GET","https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e3928217ff6074698f148e595b686f23", true);
+weatherRequest.open("GET","https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=e3928217ff6074698f148e595b686f23&units=imperial", true);
 weatherRequest.send();
 weatherRequest.onload = function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
-  
