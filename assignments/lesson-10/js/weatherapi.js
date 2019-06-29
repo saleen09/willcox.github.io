7584b32b3a82e46f1f1a7f1e69d144b497359e01
saleen09 <saleen09@hotmail.com>
2019-06-29 10:39:15 -0700

    1

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -6,10 +6,11 @@ weatherRequest.onload = function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('cCond').innerHTML = weatherData.weather.o.main;
+    document.getElementById('cTemp').innerHTML = weatherData.main.temp;
     document.getElementById('hTemp').innerHTML = weatherData.main.temp_max;
     document.getElementById('humidity').innerHTML = weatherData.main.humidity;
     document.getElementById('wSpeed').innerHTML = weatherData.wind.speed;
+    document.getElementById('cCond').innerHTML = weatherData.weather.o.main;
 
 
 }
